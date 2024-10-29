4bbd91fa173064e4e89a03df0cdbf23b100f30eb
ElBendo <plbond@mail.ru>
2024-10-29 22:14:42 +0300

    First version of the simple RESTful Spring web server.

---
+++ CoffeeApp_v1/src/test/java/el/bendo/spring/CoffeeApp/CoffeeAppTests.java
@@ -0,0 +1,13 @@
+package el.bendo.spring.CoffeeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
